66fd8efedba991ccb7cc96c65a2a23bc35ec12b3
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-27 17:52:19 +0700

    mendeklarasikan interface DestinasiNavigasi untuk menyetandari (as template) route dan title pada setiap navigasi

---
+++ app/src/main/java/com/example/room_database/view/route/DestinasiNavigasi.kt
@@ -0,0 +1,7 @@
+package com.example.room_database.view.route
+interface DestinasiNavigasi{
+    //nama unik utk menentukan jalur utk composable
+    val route: String
+    //    string resource id yang berisi judul yang akan ditampilkan di layar halaman
+    val titleRes: Int
+}
