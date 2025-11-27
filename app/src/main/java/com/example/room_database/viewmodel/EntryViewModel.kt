d8ac431703087f66466e254304aea79080390272
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-27 16:12:33 +0700

    add class EntryViewModel dengan parameter yang sekaligus sebagai properties repositoriSiswa pada class ini mempunyai beberapa variabel dan fun yaitu var uiStateSiswa, fun validasiinput digunakan untuk mengecek apakah inputan pada masinng2 form sudah valid atau belum. fun updateUiState untuk memperbarui data setiap ada perubahan dari user trus juga ngecek valid atau enggaknya inputannya dengan manggil function validasi, fun saveSiswa ini untuk menyimpan data jika validasiinputnya valid maka datanya akan tersimpan (bisa ditambahkan) ke dalam database

--- app/src/main/java/com/example/room_database/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/room_database/viewmodel/EntryViewModel.kt
@@ -1,6 +1,31 @@
 package com.example.room_database.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.room_database.repositori.RepositoriSiswa
 import com.example.room_database.room.Siswa
+
+class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+        var uiStateSiswa by mutableStateOf(UIStateSiswa())
+            private set
+        private fun validasiInput (uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa=
+            UIStateSiswa(detailSiswa= detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+    suspend fun saveSiswa(){
+        if(validasiInput()){
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
 
 data class UIStateSiswa(
     val detailSiswa: DetailSiswa= DetailSiswa(),
