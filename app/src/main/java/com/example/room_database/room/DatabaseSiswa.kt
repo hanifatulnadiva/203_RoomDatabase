d07e39ea9fc4f3cfe3271d4fa9c9b6d54785a406
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-27 12:10:40 +0700

    add setup Room Database untuk menjalankan query2 yang ada di fun SiswaDao

---
+++ app/src/main/java/com/example/room_database/room/DatabaseSiswa.kt
@@ -0,0 +1,24 @@
+package com.example.room_database.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database (entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa: RoomDatabase(){
+    abstract  fun siswaDao(): SiswaDao
+
+    companion object{
+        @Volatile
+        private var Instance : DatabaseSiswa? = null
+        fun getDatabase(context: Context): DatabaseSiswa{
+            return (Instance?: synchronized(lock= this){
+              Room.databaseBuilder(
+                  context, klass=DatabaseSiswa::class.java,
+                  name="Siswa_database")
+                  .build().also{ Instance= it}
+            })
+        }
+    }
+}
