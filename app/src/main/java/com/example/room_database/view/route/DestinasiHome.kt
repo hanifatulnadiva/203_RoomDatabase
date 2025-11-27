71679681876dca89ce01a8d4eac1333ef8b59247
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-27 17:53:47 +0700

    add  object DestinationHome

---
+++ app/src/main/java/com/example/room_database/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.room_database.view.route
+
+import com.example.room_database.R
+object DestinasiHome {
+    object DestinasiHome : DestinasiNavigasi{
+        override val route = "home"
+        override val titleRes = R.string.app_name
+    }
+}
