1f0c4598aa3011c35d05cefe02e91587a122231f
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-27 12:50:47 +0700

    Menambahkan interface ContainerApp untuk deklarasi fun, class ContainerDataApp untuk implementasi dari interface , dan class ApplikasiSiswa untuk mengelola penyediaan dependensi di aplikasi. ContainerDataApp berperan sebagai pembungkus yang membuat dan menyediakan instance RepositoriSiswa melalui SiswaDao. Instance container kemudian disimpan di ApplikasiSiswa agar dapat diakses oleh class lain secara global

---
+++ app/src/main/java/com/example/room_database/repositori/ContainerApp.kt
@@ -0,0 +1,24 @@
+package com.example.room_database.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.room_database.room.DatabaseSiswa
+
+interface ContainerApp{
+    val repositoriSiswa: RepositoriSiswa
+}
+class ContainerDataApp(private val context: Context):
+        ContainerApp{
+            override val repositoriSiswa: RepositoriSiswa by lazy {
+                OfflineRepositoriSiswa(
+                    siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+                )
+            }
+        }
+class ApplikasiSiswa: Application(){
+    lateinit var container: ContainerApp
+    override  fun onCreate(){
+        super.onCreate()
+        container= ContainerDataApp(this)
+    }
+}
