d8048c2efc1d958f89108387c90240bfd6d0ff46
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-27 16:50:40 +0700

    add fun SiswaTopAppBar di file AppBar digunakan untuk membuat top app dalam setiap halaman agar bisa digunakan berulanng2 kalii dengan memanggil functionnya. function ini memiliki beberapa parameter yaitu title untuk judul dari topbarnya dgn tipe String, lalu canNavigate, modifier, scrollBehavior dan navigateUp

--- app/src/main/java/com/example/room_database/view/AppBar.kt
+++ app/src/main/java/com/example/room_database/view/AppBar.kt
@@ -1,2 +1,20 @@
 package com.example.room_database.view
 
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title:String,
+    canNavigateBack:Boolean,
+    modifier:Modifier= Modifier,
+    scrollBehavior: TopAppBarScrollBehavior?= null,
+    navigateUp:()->Unit = {}
+){
+
+}
