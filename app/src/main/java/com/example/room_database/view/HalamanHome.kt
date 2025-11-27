2c0f549c6b938d4ec4f4fbb1051ec3db352d5aee
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-27 17:12:42 +0700

    add fun DataSiswa dengan parameter siswa yang mengakses class siswa yang ada di folder room dan modifier

--- app/src/main/java/com/example/room_database/view/HalamanHome.kt
+++ app/src/main/java/com/example/room_database/view/HalamanHome.kt
@@ -1,9 +1,15 @@
 package com.example.room_database.view
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.room_database.room.Siswa
 import com.example.room_database.viewmodel.HomeViewModel
 import com.example.room_database.viewmodel.provider.PenyediaViewModel
 
@@ -13,6 +19,12 @@ fun HomeScreen(
     navigateToItemEntry:()->Unit,
     modifier:Modifier= Modifier,
     viewModel: HomeViewModel= viewModel(factory = PenyediaViewModel.Factory)
+){
+}
+
+fun DataSiswa(
+    siswa: Siswa,
+    modifier:Modifier = Modifier
 ){
 
 }
