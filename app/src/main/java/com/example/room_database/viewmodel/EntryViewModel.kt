a9a155a9332b19ff723ff27dd5d88e4af077d97e
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-27 15:38:42 +0700

    add data class UIStateSiswa sebagai tempat untuk menyimpan data saat berada di ui. toUIStateSiswa() Digunakan saat mau menampilkan data dari database ke UI. toDetailSiswa() digunakan untuk data bisa diedit dan bisa ditampilkan di form

--- app/src/main/java/com/example/room_database/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/room_database/viewmodel/EntryViewModel.kt
@@ -2,6 +2,10 @@ package com.example.room_database.viewmodel
 
 import com.example.room_database.room.Siswa
 
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa= DetailSiswa(),
+    val isEntryValid: Boolean= false
+)
 data class DetailSiswa(
     val id: Int = 0,
     val nama:String="",
@@ -14,5 +18,16 @@ fun DetailSiswa.toSiswa(): Siswa =Siswa(
     nama = nama,
     alamat = alamat,
     telpon = telpon
+)
+
+fun Siswa.toUIStateSiswa(isEntryValid: Boolean= false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this. toDetailSiswa(),
+    isEntryValid=isEntryValid
+)
+fun Siswa.toDetailSiswa(): DetailSiswa= DetailSiswa(
+    id=id,
+    nama=nama,
+    alamat= alamat,
+    telpon= telpon
 )
 
