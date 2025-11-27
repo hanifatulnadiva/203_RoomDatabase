4a1f4dfb2f42e0acd71e96730f2546615d09c51f
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-27 15:18:02 +0700

    menambahkan data class DetailSiswa sebagai model untuk layer UI

---
+++ app/src/main/java/com/example/room_database/viewmodel/EntryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.room_database.viewmodel
+
+import com.example.room_database.room.Siswa
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama:String="",
+    val alamat:String="",
+    val telpon : String =""
+)
+
+
+
