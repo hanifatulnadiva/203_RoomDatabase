2cb21739c90af8635a1423f5137e11051b124cd4
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-27 17:15:54 +0700

    add Composable dan menambahka layout Card di data siswa  dan membuatt elevation agar card nya muncul

--- app/src/main/java/com/example/room_database/view/HalamanHome.kt
+++ app/src/main/java/com/example/room_database/view/HalamanHome.kt
@@ -2,12 +2,15 @@ package com.example.room_database.view
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.room_database.room.Siswa
 import com.example.room_database.viewmodel.HomeViewModel
@@ -22,9 +25,14 @@ fun HomeScreen(
 ){
 }
 
+@Composable
 fun DataSiswa(
     siswa: Siswa,
     modifier:Modifier = Modifier
 ){
+    Card (modifier = Modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ){
 
+    }
 }
