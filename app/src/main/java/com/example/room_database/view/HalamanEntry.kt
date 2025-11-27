a40fd46bfcf9c5d50c05bf4f6b60675787aaa601
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-27 18:07:46 +0700

    add fun EntrySiswaBody dengan parameter uiStataeSiswa, onSiswaValueChange onSaveClick dan modifier. disini beriisi Layout Column dengan style yang sudah di atur terus memanggil function FormInputSiswa dan menambahkan button untuk submit

--- app/src/main/java/com/example/room_database/view/HalamanEntry.kt
+++ app/src/main/java/com/example/room_database/view/HalamanEntry.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,9 +16,34 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.example.room_database.viewmodel.DetailSiswa
 import com.example.room_database.R
+import com.example.room_database.viewmodel.UIStateSiswa
 
 
-
+@Composable
+fun EntrySiswaBody(
+    uiStateSiswa: UIStateSiswa,
+    onSiswaValueChange: (DetailSiswa)-> Unit,
+    onSaveClick: ()-> Unit,
+    modifier: Modifier = Modifier
+)
+{
+    Column(verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(R.dimen.padding_medium))) {
+        FormInputSiswa(
+            detailSiswa = uiStateSiswa.detailSiswa,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            enabled = uiStateSiswa.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.btn_submit))
+        }
+    }
+}
 @Composable
 fun FormInputSiswa(
     detailSiswa: DetailSiswa,
