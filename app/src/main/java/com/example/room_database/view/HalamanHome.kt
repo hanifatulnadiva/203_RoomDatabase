e950c099d3c3daf6aaed01f5da93a9bcf68966f0
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-27 17:28:13 +0700

    add nama dan telpon di susun secara horizontal dan nambahin icon phone

--- app/src/main/java/com/example/room_database/view/HalamanHome.kt
+++ app/src/main/java/com/example/room_database/view/HalamanHome.kt
@@ -3,17 +3,24 @@ package com.example.room_database.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -46,7 +53,21 @@ fun DataSiswa(
         ){
             Row (modifier= Modifier
                 .fillMaxWidth()
-            ){  }
+            ){
+                Text(
+                    text=siswa.nama,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(modifier = Modifier .weight(weight = 1f))
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                )
+                Text(
+                    text = siswa.telpon,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
 }
