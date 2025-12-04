0b7c0cf63f9bdc13625c3990568223b71a3cdf5c
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-12-04 14:19:38 +0700

    add DetaiViewmodel dan editviewmodel ke PenyediaViewModel.kt

--- app/src/main/java/com/example/room_database/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/room_database/viewmodel/provider/PenyediaViewModel.kt
@@ -3,22 +3,33 @@ package com.example.room_database.viewmodel.provider
 import android.widget.VideoView
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.room_database.repositori.ApplikasiSiswa
+import com.example.room_database.viewmodel.DetailViewModel
+import com.example.room_database.viewmodel.EditViewModel
 import com.example.room_database.viewmodel.EntryViewModel
 import com.example.room_database.viewmodel.HomeViewModel
 
-object PenyediaViewModel{
+object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer{
+        initializer {
             HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+        //tambahan
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EditViewModel(this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa)
+        }
     }
 }
+
 fun CreationExtras.aplikasiSiswa(): ApplikasiSiswa=
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as ApplikasiSiswa)
